22cef31d33a4dad5db308970196cf98c5a53c2be
Alan Alves <125587710+AlanAlves0@users.noreply.github.com>
2024-05-20 14:00:19 -0300

    adicionando if e else

---
+++ app.js
@@ -0,0 +1,28 @@
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = 9;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1; 
+
+//enquanto chute não for igual ao numero secreto
+while(chute != numeroSecreto){
+    chute = prompt("Escolha um número entre 1 e 10");
+    // se chuter for igual ao numero secreto
+    if(numeroSecreto == chute){
+        break;
+    } else{
+        if( chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        }else{
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
+    }
+}
+if(tentativas > 1){
+    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+}else{
+    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+}
+
